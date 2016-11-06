20c030c7bc00c765da6e7ca988be5c656cd470c8
Aaron <aaronvine@gmail.com>
2016-11-07 00:22:11 +0200

    progress routes and db

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,32 @@
 module.exports = function (app, passport) {
 
+    app.get('/', function (req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/login', function (req, res) {
+        res.render('login.ejs');
+    });
+
+    app.get('/signup', function (req, res) {
+        res.render('signup.ejs');
+    });
+
+    app.get('/profile', function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user
+        });
+    });
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.render('login.ejs');
+    });
 };
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
